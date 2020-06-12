34474dede90ee4fcbdb7cbc2ac23ecc3ea0923cc
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2020-06-12 10:47:08 +0200

    load orjson, ujson or json

--- json_streams/jsonlib.py
+++ json_streams/jsonlib.py
@@ -1,14 +1,25 @@
 """
 Utility library to load the underlying json library.
 
-Imports `ujson` if it is present, otherwise it imports `json` from
+Imports `orjson` or `ujson` if either is present, otherwise it imports `json` from
 the standard library.
 """
-from orjson import dumps, loads
-# try:
-#     from ujson import dump, dumps, load, loads  # pylint: disable=unused-import
-# except ImportError:
-#     from json import dump, dumps, load, loads  # noqa: F401
+try:
+    from orjson import dumps, loads
+
+    PREFERRED_READ_MODE = "br"
+    PREFERRED_WRITE_MODE = "bw"
+except ImportError:
+    try:
+        from ujson import dumps, loads  # pylint: disable=unused-import
+
+        PREFERRED_READ_MODE = "br"
+        PREFERRED_WRITE_MODE = "bw"
+    except ImportError:
+        from json import dumps, loads  # noqa: F401
+
+        PREFERRED_READ_MODE = "r"
+        PREFERRED_WRITE_MODE = "w"
 
 
 def load_from_file(file_name: str):
@@ -18,7 +29,7 @@ def load_from_file(file_name: str):
     :param file_name: name of the file to load from.
     :return: the loaded JSON file.
     """
-    with open(file_name, 'br') as fp:
+    with open(file_name, PREFERRED_READ_MODE) as fp:
         return loads(fp.read())
 
 
@@ -30,5 +41,5 @@ def dump_to_file(obj, file_name: str):
     :param file_name: name of the file to dump to.
     :return: anything returned from the backend.
     """
-    with open(file_name, 'bw') as fp:
+    with open(file_name, PREFERRED_WRITE_MODE) as fp:
         return fp.write(dumps(obj))
