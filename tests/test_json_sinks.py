b67281b64b84398a097e0d77f7e5691201bcbf14
Kristoffer Andersson <kristoffer.andersson@gu.se>
2021-05-18 11:53:50 +0200

    test: clean tests

--- tests/test_json_sinks.py
+++ tests/test_json_sinks.py
@@ -11,13 +11,9 @@ DATA = [
     {"a": 2},
 ]
 
-JSON_FACIT = b'[\n{"a":1},\n{"a":2}\n]'
-JSONL_FACIT = b'{"a":1}\n{"a":2}\n'
-
 
 def gen_data():
-    for i in DATA:
-        yield i
+    yield from DATA
 
 
 @pytest.mark.parametrize(
