2f84595cdfd09adc242a59c3a24eb6651515597a
Kristoffer Andersson <kristoffer.andersson@gu.se>
2021-06-17 14:02:20 +0200

    style: use different Pathlike on different python

--- json_streams/utility/types.py
+++ json_streams/utility/types.py
@@ -1,6 +1,12 @@
 import os
+import sys
 import typing
 
 # pylint: disable=unsubscriptable-object
 
-Pathlike = typing.TypeVar("Pathlike", str, bytes, os.PathLike[str], os.PathLike[bytes])
+if sys.version_info >= (3, 9):
+    Pathlike = typing.TypeVar(
+        "Pathlike", str, bytes, os.PathLike[str], os.PathLike[bytes]
+    )
+else:
+    Pathlike = typing.TypeVar("Pathlike", str, bytes, os.PathLike)
