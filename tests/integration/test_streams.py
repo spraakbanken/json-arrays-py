b671053807775da5fca2302a2193ec563515027d
Kristoffer Andersson <kod.kristoff@gmail.com>
2021-11-13 18:48:34 +0100

    test: add streaming test

---
+++ tests/integration/test_streams.py
@@ -0,0 +1,10 @@
+import pytest
+import json_streams
+
+@pytest.mark.parametrize('outfile', ['tests/data/gen/array.json', 'tests/data/gen/array.jsonl'])
+@pytest.mark.parametrize('infile', ['tests/data/array.json', 'tests/data/array.jsonl'])
+def test_streaming(infile, outfile):
+    json_streams.dump_to_file(
+        json_streams.load_from_file(infile),
+        outfile,
+    )
