7555319d82c60c8803e2a7394646a2d5f17ffe07
Kristoffer Andersson <kod.kristoff@gmail.com>
2022-05-26 08:49:05 +0200

    fix: fix failing type checks

--- json_streams/file.py
+++ json_streams/file.py
@@ -8,7 +8,7 @@ from json_streams import types, utility
 def open(file_name: types.Pathlike, mode = "rb") -> types.File:
     assert "b" in mode
     if utility.is_gzip(file_name):
-        return gzip.GzipFile(file_name, mode)
+        return gzip.GzipFile(file_name, mode)  # type: ignore
     else:
         return builtins.open(file_name, mode)
 
