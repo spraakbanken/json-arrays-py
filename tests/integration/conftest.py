ba10bc8b25cb0dc8945e38112fbcc8600a249857
Kristoffer Andersson <kristoffer.andersson@gu.se>
2023-03-10 11:28:59 +0100

    test: use typing.List and typing.Dict

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,8 +1,10 @@
+from typing import Dict, List
+
 import pytest
 
 
 @pytest.fixture(name="entries")
-def fixture_entries() -> list[dict]:
+def fixture_entries() -> List[Dict]:
     return [
         {"a": "b"},
         {"a": {"b": 2}},
