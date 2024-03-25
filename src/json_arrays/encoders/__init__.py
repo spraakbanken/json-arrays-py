9065fdc3a86fdda317ee89f22e546d99c31860ec
Kristoffer Andersson <kristoffer.andersson@gu.se>
2024-03-25 11:52:03 +0100

    docs: add doc-test for encoders

--- src/json_arrays/encoders/__init__.py
+++ src/json_arrays/encoders/__init__.py
@@ -1,3 +1,12 @@
+"""Encoders
+
+`encode_decimal`
+>>> from decimal import Decimal
+>>> from json_arrays import jsonlib, encoders
+>>> jsonlib.dumps([10.20, "10.20", Decimal("10.20")], default=encoders.encode_decimal)
+b'[10.20,"10.20",10.20]'
+"""
+
 from .decimal_encoders import encode_decimal
 
 __all__ = ["encode_decimal"]
