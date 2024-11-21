6ace046d820d27632b7067e498746ddca29549d5
Kristoffer Andersson <kristoffer.andersson@gu.se>
2024-11-21 15:00:56 +0100

    style: sort some imports

--- tests/test_iter.py
+++ tests/test_iter.py
@@ -2,8 +2,8 @@ import io
 import json
 
 import pytest
-from json_arrays import json_iter, jsonl_iter
 
+from json_arrays import json_iter, jsonl_iter
 from tests.utils import compare_iters
 
 DATA = [
