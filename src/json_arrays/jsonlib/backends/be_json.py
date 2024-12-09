dd6b28170396d91df2e97d57d441cd175d9da705
Kristoffer Andersson <kristoffer.andersson@gu.se>
2024-12-09 14:50:54 +0100

    fix: use json.loads directly in be_json

--- src/json_arrays/jsonlib/backends/be_json.py
+++ src/json_arrays/jsonlib/backends/be_json.py
@@ -1,12 +1,10 @@
 """Json backend using standard `json` lib."""
 
 import json
-from typing import Any, Union
 
 
 def dumps(obj, **kwargs) -> bytes:
     return json.dumps(obj, separators=(",", ":"), ensure_ascii=False, **kwargs).encode("utf-8")
 
 
-def loads(s: Union[bytes, bytearray, str], **kwargs) -> Any:
-    return json.loads(s.decode("utf-8") if isinstance(s, (bytes, bytearray)) else s, **kwargs)
+loads = json.loads
